f5f3d47f9a187800153a3241afc2a1760bb33ac5
HarmonyRegan <harmonyjane@live.com>
2023-02-01 10:08:41 +0000

    Basic Login form inplemented on Page.js. Requires CSS

--- frontend/client/app/page.js
+++ frontend/client/app/page.js
@@ -1,8 +1,46 @@
-export default function login() {
+import { React } from "react";
+import "./login.css"
+
+export default function login(props) {
   return (
-  <div>
-    <h1>Login</h1>
-    <h1>Hello</h1>
+    <div className="container">
+      {/* <div id="tabs">
+            <button className = "tab-links active" id="login-btn">Login</button>
+            <button className = "tab-links" id="signup-btn">Sign Up</button>
+      </div>  */}
+      <div className="main-content"></div>
+        <form className="loginForm">
+          <div className="loginContent">
+            <h3 className="loginTitle">Login</h3>
+          <div className="form">
+              <label>Username: </label>
+              <input
+              type="userName"
+              className="usernameInput"
+              placeholder="Enter Username"
+              />
+          </div>
+          <div className="form">
+              <label>Email Address: </label>
+              <input
+              type="email"
+              className="emailInput"
+              placeholder="Enter Email"
+              />
+          </div>
+          <div className="form">
+              <label>Password: </label>
+              <input
+              type="password"
+              className="passwordInput"
+              placeholder="Enter Password"
+              />
+          </div>
+          <div className="LogInBtn">
+            <button type="submit" className="LoginBtn">Login</button>
+          </div>
+        </div>
+      </form>
     </div>
   )
 }
