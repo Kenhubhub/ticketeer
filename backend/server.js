3a5fd30e42d50680379f62d688ff5f238838ee57
HarmonyRegan <harmonyjane@live.com>
2023-02-03 11:25:33 +0000

    Added Authentication to Register Route, allows creation of users

--- backend/server.js
+++ backend/server.js
@@ -7,15 +7,30 @@ app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 const {connectDb} = require("./db/db")
 connectDb(process.env.MONGO_URI);
+const UserDB = require ('./schemas/userSchema');
+
 // login route
 app.post("/login",(req,res)=>{
     res.json("success");
 })
 
 // register route
-app.post("/register",(req,res)=>{
+app.post("/register",async (req,res)=>{
     const {username,password,email} = req.body;
-    res.json({username,password,email})
+
+    let document = await UserDB.findOne({email});
+
+    if(document){
+        res.json({status: false,message:'Email already exists'});
+    }
+
+    const user = await UserDB.create({
+        username,
+        email,
+        password
+    });
+
+    res.json({status:true, message:user});  
 })
 app.listen(port, ()=>{
     console.log(`listening on ${port}`)
