6bf0b8e32c852da0a840026ec392915b082af2e6
Kenhubhub <98609221+Kenhubhub@users.noreply.github.com>
2023-02-09 16:04:31 +0000

    I did a whole lot of stuff thanks for the help:)

--- frontend/client/components/Event.jsx
+++ frontend/client/components/Event.jsx
@@ -1,12 +1,20 @@
 import { useRouter } from "next/navigation";
 
-
+const getCustomerPurchases = async (id)=>{
+    
+    const res = await fetch('http://localhost:4000/user/getPurchases', {method: 'POST',headers: {'Content-Type': 'application/json'},body:JSON.stringify({id})})
+      
+    const data = await res.json()
+     return data
+}
 const Event = ({event,id}) => {
     const router = useRouter()
-    const BuyEvent = async (e,id,event) =>{
+    const BuyEvent = async (e,id,event,ta) =>{
         e.preventDefault()
         
-        
+      
+       const purchases = await getCustomerPurchases(id);
+       
         const sendData = {
             id,
             name: event.name,
@@ -14,13 +22,22 @@ const Event = ({event,id}) => {
             price: event.price,
             date:  new Date().toLocaleDateString('en-GB')
         }
-        const res = await fetch('http://localhost:4000/user/eventtouser', {method: 'POST',headers: {'Content-Type': 'application/json'},body:JSON.stringify(sendData)})
-        const data = await res.json();
-        console.log("successfully bought", data)
-        //update tickets available
-        const resforta = await fetch('http://localhost:4000/events/ticketchange', {method: 'POST',headers: {'Content-Type': 'application/json'},body:JSON.stringify({id: event._id,ta: event.ta-1})})
-        console.log("ticket available updated",resforta)
-        router.push(`/customer/${id}`)
+        let numb = 0;
+        purchases.forEach(purchase => {
+            if(purchase.name === sendData.name && purchase.location === sendData.location && purchase.price === sendData.price && purchase.date === sendData.date){
+                numb++;
+            }
+        })
+        console.log(`number of identical tickets === ${numb}`)
+        if(numb < ta){
+            const res = await fetch('http://localhost:4000/user/eventtouser', {method: 'POST',headers: {'Content-Type': 'application/json'},body:JSON.stringify(sendData)})
+            const data = await res.json();
+            console.log("successfully bought", data)
+            //update tickets available
+            const resforta = await fetch('http://localhost:4000/events/ticketchange', {method: 'POST',headers: {'Content-Type': 'application/json'},body:JSON.stringify({id: event._id,ta: event.ta-1})})
+            console.log("ticket available updated",resforta)
+            router.push(`/customer/${id}`)
+        }
     }
     return (
         <>
@@ -35,7 +52,7 @@ const Event = ({event,id}) => {
                         <li>Tickets per use: {event.maxper}</li>
                     </ul>
                     {
-                        event.ta == 0 ? <p>Sold out</p> : <button onClick = {(e)=>BuyEvent(e,id,event)}type="submit">Buy</button> 
+                        event.ta == 0 ? <p>Sold out</p> : <button onClick = {(e)=>BuyEvent(e,id,event,event.maxper)}type="submit">Buy</button> 
                     }
                     
                 </div>
